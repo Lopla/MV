8e071c56430af35ea725646743a9eb281201289f
Pawel Galezowski <pawel.galezowski@tfbank.pl>
2022-07-17 21:51:05 +0200

    added libs

---
+++ libs/MV/Base/BaseManifest.cs
@@ -0,0 +1,12 @@
+﻿using MV.Interfaces;
+
+namespace MV.Base;
+public abstract class BaseManifest : IManifest
+{
+    public string GetName()
+    {
+        return this.OnGetName();
+    }
+
+    public abstract string OnGetName();
+}
---
+++ libs/MV/Interfaces/IManifest.cs
@@ -0,0 +1,6 @@
+namespace MV.Interfaces;
+
+public interface IManifest
+{
+    string GetName();
+}
